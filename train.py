78aafccb78e5eca1be8ca232cd9c5784c4604e50
Gabriel Melendez <56122852+gabrielmelendez779@users.noreply.github.com>
2021-09-10 12:26:50 -0300

    Update train.py

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ from sklearn.datasets import load_iris
 def train():
     # Load directory paths for persisting model
 
-    MODEL_DIR = os.environ["MODEL_DIR"]
-    MODEL_FILE = os.environ["MODEL_FILE"]
+    MODEL_DIR = "/home/my-model/"
+    MODEL_FILE = "model"
     MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
 
     # Load and split the data
